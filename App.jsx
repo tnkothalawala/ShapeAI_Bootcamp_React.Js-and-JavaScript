95dcac598738c041664ce6d5c74a600538bb78f9
tnkothalawala <86547929+tnkothalawala@users.noreply.github.com>
2021-06-27 12:43:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "./Header";
+import Information from "./Information";
+import Footer from "./Footer";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Information />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ Information.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Information() {
+  return (
+    <div className="note">
+      <h2>Javascript,React.Js </h2>
+      <p>
+        <h4>A basic front end Development with Javascript and React.Js </h4>
+      </p>
+      <p>
+        <h5>
+          Introduction to programming in JavaScript & Introduction to HTML
+          ,foundational knowledge in React.Js and a handon Project are the
+          topics to be covered.
+        </h5>
+      </p>
+    </div>
+  );
+}
+
+export default Information;
